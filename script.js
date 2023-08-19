2e77f4f2004567c247421c567ac8de9c2b99d14a
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2023-08-18 22:13:57 -0400

    added color dropdown/ changed add to cart

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ AOS.init({
     duration: 1000, 
     delay: 200,  
   });
-  
+
+  function changeButtonColor() {
+    const btn = document.querySelector('.btn-primary');
+    btn.style.backgroundColor = 'black';
+    btn.style.color = 'white';
+    btn.removeEventListener('mouseover', hoverEffect);
+    btn.removeEventListener('mouseout', hoverEffect);
+}
+
+function hoverEffect() {
+    const btn = document.querySelector('.btn-primary');
+    btn.style.backgroundColor = btn.style.backgroundColor === 'black' ? 'white' : 'black';
+    btn.style.color = btn.style.color === 'white' ? 'black' : 'white';
+}
+
+document.querySelector('.btn-primary').addEventListener('mouseover', hoverEffect);
+document.querySelector('.btn-primary').addEventListener('mouseout', hoverEffect);
