1d8bba0d85042362242d29094e680c4991dad161
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2023-08-22 13:04:09 -0400

    changed hero img

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ AOS.init({
     clickedThumbnail.classList.add('thumbnail-active');
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  zoom.setup();
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) { // Adjust the value as needed
+      navbar.classList.add('navbar-scrolled');
+  } else {
+      navbar.classList.remove('navbar-scrolled');
+  }
 });
