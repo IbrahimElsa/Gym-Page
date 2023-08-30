50641d3d209cb1b953157d56762058d91192f993
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2023-08-29 20:09:37 -0400

    dotenv return

--- functions/get_product/get_product.js
+++ functions/get_product/get_product.js
@@ -1,6 +1,6 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 import { MongoClient } from 'mongodb';
-// require('dotenv').config();
+require('dotenv').config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const DB_NAME = 'GymDatabase'; // replace with your database name
