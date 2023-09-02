a03c48d772f7c0151c286c7d6d8301138521551d
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2023-09-01 22:34:01 -0400

    move script :)

--- dynamicRouting.js
+++ dynamicRouting.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const productId = urlParams.get('id');
 
-    console.log("Product ID:", productId);
+    const titleElement = document.getElementById('product-title');
+    const priceElement = document.getElementById('product-price');
+    const descriptionElement = document.getElementById('product-description');
 
     if (productId === null) {
         console.error("No product ID provided in the URL.");
@@ -21,5 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.error("No data found for this product ID.");
             }
         })
-        
+        .catch(err => {
+            console.error("Failed to fetch product:", err);
+        });
 });
